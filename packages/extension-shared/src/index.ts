a6f68581a76450d7d496420d87abf5ae14538b22
zzggo <han.zhang@flowfoundation.org>
2025-07-24 11:30:08 +1000

    fixed: remove any chrome storage related code outside extension-shared
    Closes #8

--- packages/extension-shared/src/index.ts
+++ packages/extension-shared/src/index.ts
@@ -1,6 +1,6 @@
 export * from './chrome-logger';
 export * from './message';
 export * from './messaging';
-export * from './storage';
+export * from './chrome-storage';
 export * from './contact-utils';
 export * from './retryOperation';
--- packages/extension-shared/src/storage.ts
+++
@@ -1,9 +0,0 @@
-import { chromeStorage } from './chrome-storage';
-
-// Have a sotrage instance exported from here to avoid errors in extension
-// Extension should update the storage instance from @onflow/frw-extension-shared/storage to @onflow/frw-data-model if needed
-export type { Storage, StorageConfig, StorageChange, AreaName } from '@onflow/frw-data-model';
-
-// Export for manual initialization
-export { initializeStorage, memoryStorage } from '@onflow/frw-data-model';
-export { chromeStorage };
