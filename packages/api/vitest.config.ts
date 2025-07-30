27ed13fc1469008fed3ff7f4e1e3d712b56b3a33
zzggo <han.zhang@flowfoundation.org>
2025-07-30 10:10:18 +1000

    feat: test file update
    Closes #18

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)', '__tests__/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     exclude: ['**/node_modules/**', '**/dist/**', '**/.{idea,git,cache,output,temp}/**'],
     globals: true,
     coverage: {
